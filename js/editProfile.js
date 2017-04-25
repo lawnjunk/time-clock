a7845f33cf15d1c3653333dc03f1246c184e9702
Spencer Gietzen <Speng1516@comcast.net>
2017-04-25 13:57:53 -0700

    editProfile page

--- js/editProfile.js
+++ js/editProfile.js
@@ -3,6 +3,22 @@
 var form = document.getElementById('profile-form');
 form.addEventListener('submit', updateProfile);
 
+function populateTextBoxes() {
+  try {
+    var employee = JSON.parse(localStorage.getItem('thisEmployee'));
+
+    var profilePicText = document.getElementById('profilePic');
+    var phoneNumberText = document.getElementById('phoneNumber');
+    var emailText = document.getElementById('email');
+
+    profilePicText.textContent = employee.profilePic;
+    phoneNumberText.textContent = employee.phoneNumber;
+    emailText.textContent = employee.email;
+  } catch(error) {
+    console.log(error);
+  }
+}
+
 function updateProfile(event) {
   event.preventDefault();
 
@@ -10,19 +26,30 @@ function updateProfile(event) {
   var phoneNumber = event.target.phoneNumber.value;
   var email = event.target.email.value;
 
-  var employee = JSON.parse(localStorage.getItem('thisEmployee'));
-  var employees = JSON.parse(localStorage.getItem('employees'));
+  try {
+    var employee = JSON.parse(localStorage.getItem('thisEmployee'));
+    var employees = JSON.parse(localStorage.getItem('employees'));
 
-  employee.profilePic = profilePic;
-  employee.phoneNumber = phoneNumber;
-  employee.email = email;
+    employee.profilePic = profilePic;
+    employee.phoneNumber = phoneNumber;
+    employee.email = email;
 
-  for(var i = 0; i < employees.length; i++) {
-    if(employees[i].id === employee.id) {
-      employees[i] = employee;
+    for(var i = 0; i < employees.length; i++) {
+      if(employees[i].id === employee.id) {
+        employees[i] = employee;
+      }
     }
-  }
 
-  localStorage.setItem('thisEmployee', JSON.stringify(employee));
-  localStorage.setItem('employees', JSON.stringify(employees));
+    localStorage.setItem('thisEmployee', JSON.stringify(employee));
+    localStorage.setItem('employees', JSON.stringify(employees));
+    form.clear();
+    alert('Your information has been saved.');
+    document.location.href = 'employee.html';
+  } catch(error) {
+    console.log(error);
+  }
+}
+
+if(form) {
+  populateTextBoxes();
 }
